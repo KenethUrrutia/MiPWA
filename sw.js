218ff7e4d2904d1d75df8d0643d5d1b808f63c7d
KenethUrrutia <kenethurrutia24d@gmail.com>
2025-04-23 07:07:21 -0600

    Mejorar el registro del Service Worker y agregar manejo de errores; optimizar la caché de recursos en el Service Worker.

--- sw.js
+++ sw.js
@@ -1,35 +1,45 @@
 const CACHE_NAME = `temperature-converter-v1`;
+const CACHE_URLS = [
+	'/', // index
+	'/converter.js', // css
+	'/converter.css', // js
+	'/index.html', // html
+	'/manifest.json', // manifest
+	'/icon512_rounded.png' // icon
+];
 
-// Use the install event to pre-cache all initial resources.
+// El evento 'install' se dispara cuando el Service Worker se instala por primera vez.
 self.addEventListener('install', (event) => {
+	// El Service Worker espera hasta que esté la caché
 	event.waitUntil(
 		(async () => {
 			const cache = await caches.open(CACHE_NAME);
-			cache.addAll(['/', '/converter.js', '/converter.css']);
+			cache.addAll(CACHE_URLS).catch((err) => {
+				console.error('Error al almacenar en caché', err);
+			});
 		})()
 	);
 });
 
+// El evento 'fetch' se dispara cada vez que la aplicación hace una petición de red.
 self.addEventListener('fetch', (event) => {
+	// event.respondWith() nos permite modificar la respuesta a esta petición.
 	event.respondWith(
 		(async () => {
 			const cache = await caches.open(CACHE_NAME);
-			console.log(event.request);
 
-			// Get the resource from the cache.
+			// busca en el cache una respuesta almacenada para esta URL.
 			const cachedResponse = await cache.match(event.request);
+
 			if (cachedResponse) {
 				return cachedResponse;
 			} else {
 				try {
-					// If the resource was not in the cache, try the network.
 					const fetchResponse = await fetch(event.request);
-
-					// Save the resource in the cache and return it.
 					cache.put(event.request, fetchResponse.clone());
 					return fetchResponse;
 				} catch (e) {
-					// The network failed.
+					// Error
 				}
 			}
 		})()
